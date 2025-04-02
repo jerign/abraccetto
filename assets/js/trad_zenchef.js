d75c9280e65e18a69f7b3422a739671f666de140
jeremysas-igonin <jeremy@sas-igonin.com>
2025-04-02 20:29:17 +0200

    fix reload lang was'nt working

--- assets/js/trad_zenchef.js
+++ assets/js/trad_zenchef.js
@@ -11,27 +11,49 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function ChangePlaceHolder(lang) {
-    console.log("this is the script for zenchef")
-    
+    console.log("Changing Zenchef language to:", lang);
     const zenchefWidget = document.querySelector(".zc-widget-config");
     if (zenchefWidget) {
-        zenchefWidget.setAttribute("data-lang", lang);
+        // Remove the old widget
+        const oldWidget = document.querySelector(".zc-widget-config");
+        if (oldWidget) {
+            oldWidget.remove();
+        }
+
+        // Create new widget with updated language
+        const newWidget = document.createElement("div");
+        newWidget.className = "zc-widget-config";
+        newWidget.setAttribute("data-restaurant", "372763");
+        newWidget.setAttribute("data-lang", lang);
+        
+        // Insert the new widget
+        document.head.appendChild(newWidget);
+
+        // Force Zenchef SDK to reload
+        const script = document.querySelector('script[src*="sdk.zenchef.com"]');
+        if (script) {
+            script.remove();
+            const newScript = document.createElement('script');
+            newScript.id = 'zenchef-sdk';
+            newScript.src = 'https://sdk.zenchef.com/v1/sdk.min.js';
+            newScript.async = true;
+            document.head.appendChild(newScript);
+        }
     }
-
   }
 
-  var lang = get_cookie("lang")
-
-  ChangePlaceHolder(lang)
-
-  if (lang != null) {
-    ChangePlaceHolder(lang)
+  // Initialize with current language from cookie
+  var lang = get_cookie("lang");
+  if (lang) {
+    ChangePlaceHolder(lang);
   }
 
-  document.getElementById('lang').addEventListener('change', function () {
-    ChangePlaceHolder(this.value);
-
-    document.getElementsByClassName("ZC_sdk__zc-iframe_k5FE3 ZC_sdk__opened_MKywu ZC_sdk__position-right_HkYTz ZC_sdk__zc-width-numeral_MRI1B").src += '';
-    window.location.reload();
-  });
+  // Listen for language changes
+  const langSelect = document.querySelector('select[id="lang"]');
+  if (langSelect) {
+    langSelect.addEventListener('change', function () {
+      const newLang = this.value;
+      ChangePlaceHolder(newLang);
+    });
+  }
 });
